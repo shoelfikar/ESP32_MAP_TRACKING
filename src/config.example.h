8ec78c520062e0f358f16123592e43144ce32837
sulfikardi <sulfikardi25@gmail.com>
2026-02-12 09:50:44 +0700

    update config

--- src/config.example.h
+++ src/config.example.h
@@ -13,7 +13,7 @@
 // ============================================
 // Firmware Version
 // ============================================
-#define FIRMWARE_VERSION    "2.0.0"
+#define FIRMWARE_VERSION    "1.0.0"
 #define FIRMWARE_BUILD      __DATE__ " " __TIME__
 
 // ============================================
